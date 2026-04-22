447fad82a7b07cdb82856ba83258f5f3e6dec50d
niyooshawebsites <niyooshawebsites@gmail.com>
2026-04-22 22:19:08 +0530

    changes done

--- app/forms/post.py
+++ app/forms/post.py
@@ -2,4 +2,4 @@ from  wtforms import Form, StringField, validators, TextAreaField
 
 class PostForm(Form):
     title = StringField("Title", [validators.Length(min=10, max=200), validators.DataRequired()])
-    content = TextAreaField("Content", [validators.Length(min=100), validators.DataRequired()])
+    content = TextAreaField("Content", render_kw={"id": "editor"})
